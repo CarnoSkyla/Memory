abb05615209a4990b75c59280ed9ace2463bf693
Sibusiso Labi <49154656+CarnoSkyla@users.noreply.github.com>
2022-11-29 17:32:25 +0200

    More styles and comments

--- memory/src/App.js
+++ memory/src/App.js
@@ -3,13 +3,24 @@ import React from 'react';
 import './App.css';
 
 import Registration from './Components/registration';
+import HomeScreen from './Components/home';
+import WinnerScreen from './Components/winner';
 
 function App() {
-  return (
-    <div>
-      <Registration />
-    </div>
-  );
+  if (window.document.location.href === 'http://localhost:3000/HomeScreen') {
+    return (
+      <HomeScreen/>
+    )
+  } else if(window.document.location.href === 'http://localhost:3000/WinnerScreen') {
+    return (
+      <WinnerScreen />
+    )
+  } else {
+    return (
+      <Registration/>
+    );
+  }
+  
 }
 
 export default App;
--- memory/src/Components/registration.js
+++ memory/src/Components/registration.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import '../Stylesheets/registration.css'
+import Astro1 from '../Images/a.png'
+import Astro2 from '../Images/astro2.png'
 
 const Registration = () => {
     return (
@@ -12,7 +14,23 @@ const Registration = () => {
                     <h1>Memory</h1>
                 </div>
                 <div className='container__navigation--exit-btn-wrapper'>
-                    <button className='exit-btn'>Exit Game</button>
+                    <button className='btn exit-btn'>Exit Game</button>
+                </div>
+            </div>
+            <div className='container__sub-heading-wrapper'>
+                <h2>Are you ready to play?</h2>
+            </div>
+            <div className='container__images-input-wrapper'>
+                <div className='container__images'>
+                    <img src={Astro1} alt="Astronaut" id="astro-1" width={'200'} height={'200'}/>
+                    <img src={Astro2} alt="Astronaut" id="astro-2" width={'200'} height={'200'}/>
+                </div>
+                <div className='container__input'>
+                    <input type='text' id='player-1' placeholder="Player 1"/>
+                    <input type='text' id='player-2' placeholder="Player 2"/>
+                </div>
+                <div className='container__submit'>
+                    <button type='button' onClick={() => window.location.href = "HomeScreen"} className="btn" onCl>Let's Play</button>
                 </div>
             </div>
         </div>
